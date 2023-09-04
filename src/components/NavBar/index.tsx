59bf3177539b65a2452b5ac1d2d20a92c96d2337
Pramod J M <pramodjm4@gmail.com>
2023-09-04 09:22:24 +0530

    Auto complete with the loading is added.

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,31 +1,25 @@
-import React, { useEffect, useReducer, useState } from 'react';
+import React, { useEffect, useReducer } from 'react';
+import { SyntheticEvent } from 'react';
 import { styles } from './style';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import {
+  AutocompleteChangeReason,
+  AutocompleteChangeDetails,
+} from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import ClearIcon from '@mui/icons-material/Clear';
+import CircularProgress from '@mui/material/CircularProgress';
 import Autocomplete from '@mui/material/Autocomplete';
 import { useTheme } from '@mui/material/styles';
 import { useMediaQuery } from '@mui/material';
 import { searchMovieByName } from '../../utils/Api.services';
-// import { movieName } from '../../utils/Api.services';
+
 type MovieDropDownType = {
   poster_path: string;
   title: string;
   original_language: string;
 };
-interface CountryType {
-  code: string;
-  label: string;
-  phone: string;
-  suggested?: boolean;
-}
-const countries: readonly CountryType[] = [
-  { code: 'AD', label: 'Andorra', phone: '376' },
-  {
-    code: 'AE',
-    label: 'United Arab Emirates',
-    phone: '971',
-  },
-];
 
 const NavBar = () => {
   const theme = useTheme();
@@ -33,12 +27,25 @@ const NavBar = () => {
 
   interface CategoryState {
     selectedCategory: string;
+    autocompleteValue: string;
+    extractedData: MovieDropDownType[]; // Add extractedData to the state
+    isLoading: boolean;
   }
 
-  type CategoryAction = { type: 'SELECT_MOVIES' } | { type: 'SELECT_TV_SHOWS' };
+  // Define your actions
+  type CategoryAction =
+    | { type: 'SELECT_MOVIES' }
+    | { type: 'SELECT_TV_SHOWS' }
+    | { type: 'SET_AUTOCOMPLETE_VALUE'; value: string }
+    | { type: 'SET_EXTRACTED_DATA'; data: MovieDropDownType[] }
+    | { type: 'START_LOADING' }
+    | { type: 'STOP_LOADING' }; // Action to set extractedData
 
   const initialState = {
     selectedCategory: '', // Initial selected category
+    autocompleteValue: '', // Initial value for Autocomplete
+    extractedData: [], // Initial extractedData as an empty array
+    isLoading: false,
   };
 
   const categoryReducer = (
@@ -47,14 +54,40 @@ const NavBar = () => {
   ): CategoryState => {
     switch (action.type) {
       case 'SELECT_MOVIES':
-        return { selectedCategory: 'Movies' };
+        return { ...state, selectedCategory: 'Movies' };
       case 'SELECT_TV_SHOWS':
-        return { selectedCategory: 'TV Shows' };
+        return { ...state, selectedCategory: 'TV Shows' };
+      case 'SET_AUTOCOMPLETE_VALUE':
+        return { ...state, autocompleteValue: action.value };
+      case 'SET_EXTRACTED_DATA':
+        return { ...state, extractedData: action.data };
+      case 'START_LOADING':
+        return { ...state, isLoading: true };
+      case 'STOP_LOADING':
+        return { ...state, isLoading: false };
       default:
         return state;
     }
   };
-  // Add the useReducer hook here
+
+  const handleAutocompleteChange = (
+    event: SyntheticEvent<Element, Event>,
+    value: string | MovieDropDownType | null,
+    reason: AutocompleteChangeReason,
+    details?: AutocompleteChangeDetails<any> | undefined
+  ) => {
+    if (value) {
+      if (typeof value === 'string') {
+        dispatch({ type: 'SET_AUTOCOMPLETE_VALUE', value });
+      } else {
+        const label = value.title;
+        dispatch({ type: 'SET_AUTOCOMPLETE_VALUE', value: label });
+      }
+    } else {
+      dispatch({ type: 'SET_AUTOCOMPLETE_VALUE', value: '' });
+    }
+  };
+
   const [state, dispatch] = useReducer(categoryReducer, initialState);
 
   // Function to handle the click on the "Movies" button
@@ -68,9 +101,31 @@ const NavBar = () => {
   };
 
   useEffect(() => {
-    searchMovieByName('ok');
-    // fetchTitle();
-  }, []);
+    const fetchDropdown = async () => {
+      dispatch({ type: 'START_LOADING' });
+
+      const res = await searchMovieByName(state.autocompleteValue);
+      const { results } = res;
+
+      const extractedData = results.map((result: MovieDropDownType) => ({
+        poster_path: result.poster_path || '',
+        title: result.title || '',
+        original_language: result.original_language || '',
+      }));
+
+      // Reverse the extractedData array before setting it in the state
+      const reversedData = [...extractedData].reverse();
+
+      // Set the reversed data in the state
+      dispatch({ type: 'SET_EXTRACTED_DATA', data: reversedData });
+      setTimeout(() => {
+        dispatch({ type: 'STOP_LOADING' });
+      }, 1000);
+    };
+    if (state.autocompleteValue.trim() !== '') {
+      fetchDropdown();
+    }
+  }, [state.autocompleteValue]);
 
   return (
     <>
@@ -96,10 +151,30 @@ const NavBar = () => {
                         <Autocomplete
                           id="country-select-demo"
                           sx={{ width: 300 }}
-                          options={countries}
+                          options={state.extractedData} // Use extractedData from state
                           autoHighlight
-                          getOptionLabel={(option) => option.label}
-                          renderOption={(props, option) => (
+                          getOptionLabel={(
+                            option: string | MovieDropDownType
+                          ) => {
+                            if (typeof option === 'string') {
+                              return option;
+                            } else {
+                              return option.title;
+                            }
+                          }}
+                          onChange={handleAutocompleteChange}
+                          size="small"
+                          inputValue={state.autocompleteValue}
+                          onInputChange={(event: any, newInputValue: any) => {
+                            dispatch({
+                              type: 'SET_AUTOCOMPLETE_VALUE',
+                              value: newInputValue,
+                            });
+                          }}
+                          renderOption={(
+                            props: any,
+                            option: MovieDropDownType
+                          ) => (
                             <Box
                               component="li"
                               sx={{ '& > img': { mr: 2, flexShrink: 0 } }}
@@ -108,25 +183,54 @@ const NavBar = () => {
                               <img
                                 loading="lazy"
                                 width="20"
-                                // src={`https://image.tmdb.org/t/p/w220_and_h330_face${option.poster_path}`}
+                                src={`https://image.tmdb.org/t/p/w220_and_h330_face${option.poster_path}`}
                                 alt=""
                               />
-                              {option.label}
+                              {option.title}
                             </Box>
                           )}
-                          renderInput={(params) => (
+                          renderInput={(params: any) => (
                             <TextField
                               {...params}
-                              label="Choose a country"
+                              label="Choose a movie"
+                              InputProps={{
+                                ...params.InputProps,
+                                endAdornment: (
+                                  <>
+                                    {state.isLoading ? (
+                                      <CircularProgress
+                                        color="inherit"
+                                        size={20}
+                                      />
+                                    ) : state.autocompleteValue ? (
+                                      <ClearIcon
+                                        onClick={() => {
+                                          dispatch({
+                                            type: 'SET_AUTOCOMPLETE_VALUE',
+                                            value: '',
+                                          });
+                                        }}
+                                        style={{ cursor: 'pointer' }}
+                                      />
+                                    ) : (
+                                      <SearchIcon
+                                        fontSize="small"
+                                        style={{ marginLeft: 8 }}
+                                      />
+                                    )}
+                                  </>
+                                ),
+                              }}
                               inputProps={{
                                 ...params.inputProps,
-                                autoComplete: 'new-password', // disable autocomplete and autofill
+                                autoComplete: 'new-password',
                               }}
                             />
                           )}
+                          freeSolo
                         />
                       </styles.orderLst>
-                    </styles.orderLst>{' '}
+                    </styles.orderLst>
                     <styles.orderLst>
                       <styles.button variant="contained">about</styles.button>
                     </styles.orderLst>
@@ -135,7 +239,7 @@ const NavBar = () => {
                         variant="contained"
                         onClick={handleMoviesClick}
                       >
-                        Movies{' '}
+                        Movies
                       </styles.button>
                     </styles.orderLst>
                     <styles.orderLst>
@@ -143,7 +247,7 @@ const NavBar = () => {
                         variant="contained"
                         onClick={handleTvShowsClick}
                       >
-                        Tv Shows
+                        TV Shows
                       </styles.button>
                     </styles.orderLst>
                   </styles.unOrderList>
--- src/utils/Api.services.ts
+++ src/utils/Api.services.ts
@@ -1,12 +1,11 @@
 import axios from 'axios';
 const BASEURL = process.env.REACT_APP_BASE_URL;
 export const API_KEY = process.env.REACT_APP_API_KEY;
-const token = localStorage.getItem('token') || '' || '';
 
 export const searchMovieByName = async (query: string) => {
   try {
     const response = await axios.get(
-      `/search/movie?query=${query}&api_key=${API_KEY}`
+      `${BASEURL}/search/movie?query=${query}&api_key=${API_KEY}`
     );
     return {
       status: 200,
@@ -18,6 +17,7 @@ export const searchMovieByName = async (query: string) => {
   } catch (err) {
     return {
       status: 400,
+      error: err,
       results: [],
       total_results: 0,
       message: 'the entered movie name or tv series does not exist',
